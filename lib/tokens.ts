910500e8a45784b15a6f830d773149c37ac3aba0
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-09-15 10:48:17 +0100

    feat(tokens): update USDC and USDT entries with testnet addresses and relevant documentation links

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -10,13 +10,13 @@ export type TokenInfo = {
 export const EVM_TOKENS: Record<string, TokenInfo[]> = {
   ethereum: [
     { symbol: 'ETH', name: 'Ether', logo: 'ETH', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', testnet: '' } },
-    { symbol: 'USDT', name: 'Tether USD', logo: 'USDT', addressByNetwork: { mainnet: '0xdAC17F958D2ee523a2206206994597C13D831ec7', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', testnet: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
+    { symbol: 'USDT', name: 'Tether USD', logo: 'USDT', addressByNetwork: { mainnet: '0xdAC17F958D2ee523a2206206994597C13D831ec7', testnet: '' } }, // https://tether.to/ru/supported-protocols/
     { symbol: 'WETH', name: 'Wrapped Ether', logo: 'WETH', addressByNetwork: { mainnet: '0xC02aaA39b223FE8D0a0e5C4F27eAD9083C756Cc2', testnet: '' } },
   ],
   polygon: [
     { symbol: 'MATIC', name: 'Polygon', logo: 'MATIC', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', testnet: '0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
     { symbol: 'USDT', name: 'Tether USD', logo: 'USDT', addressByNetwork: { mainnet: '0xc2132D05D31c914a87C6611C10748AaCBdA8c49B', testnet: '' } },
     { symbol: 'WETH', name: 'Wrapped Ether', logo: 'WETH', addressByNetwork: { mainnet: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', testnet: '' } },
   ],
@@ -28,19 +28,20 @@ export const EVM_TOKENS: Record<string, TokenInfo[]> = {
   ],
   arbitrum: [
     { symbol: 'ETH', name: 'Ether', logo: 'ETH', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', testnet: '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
   ],
   optimism: [
     { symbol: 'ETH', name: 'Ether', logo: 'ETH', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85', testnet: '0x5fd84259d66Cd46123540766Be93DFE6D43130D7' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
   ],
   base: [
     { symbol: 'ETH', name: 'Ether', logo: 'ETH', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', testnet: '0x036CbD53842c5426634e7929541eC2318f3dCF7e' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
   ],
   avalanche: [
     { symbol: 'AVAX', name: 'Avalanche', logo: 'AVAX', addressByNetwork: {} },
-    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', testnet: '' } },
+    { symbol: 'USDC', name: 'USD Coin', logo: 'USDC', addressByNetwork: { mainnet: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', testnet: '0x5425890298aed601595a70AB815c96711a31Bc65' } }, // https://developers.circle.com/stablecoins/usdc-contract-addresses
+    { symbol: 'USDT', name: 'Tether USD', logo: 'USDT', addressByNetwork: { mainnet: '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7', testnet: '' } }, // https://tether.to/ru/supported-protocols/
   ],
 }
 
